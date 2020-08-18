1be9447c206c64b3047c9422134b42b222279443
alohaloha <aloha_au_ia_oe_kai@yahoo.co.jp>
2020-08-18 11:28:38 +0900

    Initial commit

---
+++ pages/film/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wrap">
+    <div class="txt">「{{ films.title }}」</div>
+    <SimpleForm initial-value="初期値だよ" @submit="onSubmit"/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import SimpleForm from '~/components/SimpleForm'
+
+export default {
+  components: {
+    SimpleForm
+  },
+  computed: {
+    ...mapGetters({
+      data:state => state.title,
+    }),
+  },
+  methods: {
+    onSubmit(value) {
+      this.$store.dispatch('film/saveTxt', { text: value })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrap {
+  max-width: 40rem;
+  margin: 0 auto;
+}
+
+.txt {
+  margin: 10rem 0 2rem;
+  text-align: center;
+}
+</style>
