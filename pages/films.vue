ddfc44bb4a17e2e61896fe8b23676a01245bab2c
alohaloha <aloha_au_ia_oe_kai@yahoo.co.jp>
2020-08-19 00:44:33 +0900

    add films page

---
+++ pages/films.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <logo />
+    <h1 style="color:white">
+      {{films[0] ? films[0].title : ""}}は
+      {{films[0] ? films[0].release_date : ""}}年にできました
+    </h1>
+    <!-- <SimpleForm initial-value="初期値だよ" @submit="onSubmit" /> -->
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  computed: {
+    films(){
+      return this.$store.getters['films/films']
+    }
+},
+  mounted() {
+    this.$store.dispatch('films/fetchFilms')
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrap {
+  max-width: 40rem;
+  margin: 0 auto;
+}
+
+.txt {
+  margin: 10rem 0 2rem;
+  text-align: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="container">
-    <logo />
-    <h1 style="color:white">
-      {{films[0] ? films[0].title : ""}}は
-      {{films[0] ? films[0].release_date : ""}}年にできました
-    </h1>
-    <!-- <SimpleForm initial-value="初期値だよ" @submit="onSubmit" /> -->
+  <div>
+    <div class="imgWrap">
+      <img
+        class="img"
+        src="https://res.cloudinary.com/kiyopikko/image/upload/v1561802660/hanly-splash_ushtah.png"
+        alt
+        :width="178"
+      />
+    </div>
+    <div class="buttons">
+      <BaseButton to="/signin">ログイン</BaseButton>
+      <BaseButton to="/signup">新規登録</BaseButton>
+    </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  },
-  computed: {
-    films(){
-      return this.$store.getters['films/films']
-    }
-},
-  mounted() {
-    this.$store.dispatch('films/fetchFilms')
-  }
-}
+export default {}
 </script>
-<style lang="scss" scoped>
-.wrap {
-  max-width: 40rem;
-  margin: 0 auto;
+
+<style>
+.imgWrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  height: calc(100% - 10.25rem);
 }
 
-.txt {
-  margin: 10rem 0 2rem;
-  text-align: center;
+.buttons {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  padding: 1rem;
 }
 </style>
